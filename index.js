54cff701cc0e6c543301898cedfbba9c46d5a9f2
Harshith <harshithprakashmutta@gmail.com>
2020-07-16 16:31:40 +0300

    tetst

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const mongoose = require('mongoose');
 const postsRouter = require('./routes/posts');
 require('dotenv').config();
@@ -18,6 +19,12 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/posts', postsRouter);
+
+app.use(express.static('client/build'));
+
+app.get('/', (req, res) => {
+  res.sendFile('/index.html');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening to: ${PORT}`));
